ebcff8f7544a3d2a3e25fa1747dc941a1b28a8f3
ramirezla <ramirezluisalberto@hotmail.com>
2025-05-08 13:07:40 -0400

    API de Detección de Daños v2 requirements.txt actualizado sklearn en main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ import tensorflow as tf
 from tensorflow.keras import applications
 from fastapi import FastAPI, UploadFile, File, HTTPException
 from fastapi.responses import JSONResponse
-import scikit-learn
+import sklearn
 
 app = FastAPI(title="API de Detección de Daños", version="2.0")
 
