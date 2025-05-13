dac51dfba1f13c007b38bd98b1fb7d88f9fd7453
ramirezla <ramirezluisalberto@hotmail.com>
2025-05-13 15:59:17 -0400

    final_model_fine_tuned_v2.keras 1700 fotos thresholds personalizados top_n=2 v3.2 06

--- main.py
+++ main.py
@@ -170,7 +170,7 @@ def predict_thresholds(image_path, model, mlb_partes, mlb_danos, mlb_sugerencias
     dannos_probs = predictions[1][0]
     sugerencias_probs = predictions[2][0]
 
-    def get_top_predictions(classes, probs, label_dict, thresholds=None, top_n):
+    def get_top_predictions(classes, probs, label_dict, thresholds=None, top_n=2):
         items = []
         for i, cls in enumerate(classes):
             cls_name = label_dict.get(int(cls), f"Clase_{int(cls)}")
